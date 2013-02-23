bec4c89c35171e145457f5c4fe854298013b3276
NorfairKing <Syd.Kerckhove@gmail.com>
2013-02-23 16:20:37 +0100

    Added circleShapeTest to the test suite.

--- asteroids/test/AsteroidsTestSuite.java
+++ asteroids/test/AsteroidsTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses(
-{ PartialFacadeTest.class, VelocityTest.class })
+{VelocityTest.class, CircleShapeTest.class})
 public class AsteroidsTestSuite
 {
 
