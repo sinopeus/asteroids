5afd44b7afa3f22d7644411bb1a31b8feaef1d4c
Xavier Goás Aguililla <xavier@hal.local>
2013-02-24 02:20:54 +0100

    Added wrapper class for angles; added class for directional information.

---
+++ asteroids/Angle.java
@@ -0,0 +1,42 @@
+
+package asteroids;
+
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+import be.kuleuven.cs.som.annotate.Raw;
+
+/**
+ * A wrapper class for angles.
+ * 
+ * 
+ * @author Syd & Xavier
+ * @version 0.0
+ */
+public class Angle {
+
+    public double getAngle() {
+        return this.angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    private double angle;
+
+    public double sin() {
+        return Math.sin(angle);
+    }
+
+    public double cos() {
+        return Math.cos(angle);
+    }
+
+    public Angle (double angle) {
+        setAngle(angle % (2 * Math.PI)):
+    }
+
+    public Angle () {
+        setAngle(0.0);
+    }
+}
