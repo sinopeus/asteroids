3668b0eb7435c51331f29fe0ac0646d1f47ca586
Xavier Goás Aguililla <xavier@hal.local>
2013-02-24 22:10:02 +0100

    Fixed method descriptions.

--- asteroids/Vector.java
+++ asteroids/Vector.java
@@ -23,7 +23,7 @@ public class Vector {
      */
     @Basic
     @Raw
-    protected double getXComponent() {
+    public double getXComponent() {
        return this.x;
    }
    
@@ -35,12 +35,12 @@ public class Vector {
      */
    @Basic
    @Raw
-    protected void setXComponent(double x) {
+    public void setXComponent(double x) {
         this.x = x;
     }
 
     /**
-     * A variable for storing the x component of the vector.
+     * The x component of the vector.
      */
     protected double x;
 
@@ -52,7 +52,7 @@ public class Vector {
      */
     @Basic
     @Raw
-    protected double getYComponent() {
+    public double getYComponent() {
        return this.y;
    }
 
@@ -63,90 +63,95 @@ public class Vector {
      */
    @Basic
    @Raw
-    protected void setYComponent(double y) {
+    public void setYComponent(double y) {
         this.y = y;
     }
 
     /**
-     * A variable for storing the x component of the vector.
+     * The y component of the vector.
      */
     protected double y;
 
 
     /**
-     * Generic boolean for checking if the input can be a vector component.
+     * Checks if the input can be a vector component.
      *
      * @param x A double precision floating point number. The method will throw an exception if it isn't a double.
      * @return A boolean indicating whether the formal argument is a possible value for a vector component.
      */
-    protected boolean canHaveAsComponent(double x) {
+    public boolean canHaveAsComponent(double x) {
        return (true);
    }
 
    /**
-    * Methods implementing the basic operations on elements of commutative groups.
+    * Basic operations on elements of commutative groups.
     */
 
     /**
-     * A method which defines vectorial addition.
+     * Adds two vectors.
      *
      * @param v An instance of the class Vector which we want to add to the instance on which it is called.
      * @return A new instance of the class Vector whose components are the sums
      * of the respective components of the two Vector objects given in the
      * implicit and explicit parameter.
      */
-    protected Vector sum(Vector v) {
+    public Vector sum(Vector v) {
        //TODO
    }
 
     /**
-     * A method which defines vectorial subtraction.
+     * Subtracts one vector from another.
      *
      * @param v An instance of the class Vector which we want to subtract from the instance on which it is called.
      * @return A new instance of the class Vector whose components are the
      * differences of the respective components of the two Vector objects given
      * in the implicit and explicit parameter.
      */
-    protected Vector difference(Vector v) {
+    public Vector difference(Vector v) {
        //TODO
    }
 
     /**
-     * Methods implementing functions for commutative groups equipped with scalar multiplication.
+     * Operations on elements of commutative groups equipped with scalar
+     * multiplication.
      */
 
     /**
-     * Takes a double precision floating point number and scales the Vector
-     * object on which it is called by that factor.
+     * Scales a vector by a given factor.
      *
      * @param scalar
      * @return A new Vector object with each of the components scaled by the
      * factor provided in the explicit parameter.
      */
-    protected Vector scale(double scalar) {
+    public Vector scale(double scalar) {
        //TODO
    }
     /**
-     * Methods implementing basic operations on inner product spaces.
+     * Basic operations on elements of inner product spaces.
      */
 
     /**
-     * A method which returns the inner product of two vectors.
+     * Computes the inner product of two vectors.
      *
      * @param v A Vector object of which we want to compute the inner product with the Vector object upon which the method is called.
      * @return A double precision floating point value representing the inner product of the Vector objects in the implicit and explicit parameters.
      */
-    protected double dot(Vector v) {
+    public double dot(Vector v) {
        //TODO
    }
 
     /**
-     * A method which returns the magnitude of a vector.
+     * Computes the magnitude of a vector.
      *
      * @param v A Vector object whose magnitude we want to compute.
-     * @return A double precision floating point value representing the magnitude of the Vector object.
+     * @return A double precision floating point value representing the
+     * magnitude of the Vector object.
      */
-    protected double magnitude() {
+    public double magnitude() {
+        //TODO
+    }
+
+    public double distanceTo() {
         //TODO
     }
    
@@ -156,7 +161,7 @@ public class Vector {
      * @param y The y component of the vector we want to construct.
      **/
 
-   protected Vector(double x, double y) {
+   public Vector(double x, double y) {
        setXComponent(x);
        setYComponent(y);
    }
@@ -167,7 +172,7 @@ public class Vector {
      * {@inheritDoc}
      * @see Object#Vector()
      */
-    protected Vector() {
+    public Vector() {
        this(0, 0);
    }
 
