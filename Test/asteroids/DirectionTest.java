633522fe3a72cc1a0ae29fb99e6c6ea9aaa318c9
NorfairKing <Syd.Kerckhove@gmail.com>
2013-02-25 23:09:03 +0100

    Fixed the directionTest class

--- Test/asteroids/DirectionTest.java
+++ Test/asteroids/DirectionTest.java
@@ -1,31 +1,33 @@
 package asteroids;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class DirectionTest
 {
-    @Test
+	@Test
 	public void setUpTestAngle()
 	{
-        testAngle = new Angle(Math.PI / 2.0);
+		testAngle = new Angle(Math.PI / 2.0);
 	}
 
-    private static Angle testAngle;
+	private static Angle testAngle;
 
-    @Test
+	@Test
 	public void extendedConstructorTest_ComponentsMatchGivenComponents()
 	{
 		Direction d = new Direction(testAngle);
-        assertTrue(Util.fuzzyEquals(d.getXComponent(), testAngle.cos());
-        assertTrue(Util.fuzzyEquals(d.getYComponent(), testAngle.sin());
+		assertTrue(Util.fuzzyEquals(d.getXComponent(), testAngle.cos()));
+		assertTrue(Util.fuzzyEquals(d.getYComponent(), testAngle.sin()));
 	}
 
 	@Test
 	public void simpleConstructorTest_ComponentsMatchGivenComponents()
 	{
 		Direction d = new Direction();
-        assertTrue(Util.fuzzyEquals(d.getXComponent(), 1.0);
-        assertTrue(Util.fuzzyEquals(d.getYComponent(), 0.0);
+		assertTrue(Util.fuzzyEquals(d.getXComponent(), 1.0));
+		assertTrue(Util.fuzzyEquals(d.getYComponent(), 0.0));
 	}
-	
+
 }
