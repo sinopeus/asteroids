a9b3abf1ae304c09ffe0868bdc32d5a4ea11cd20
Xavier Goás Aguililla <xavier@dhcp-10-8-32-057.kotnet.kuleuven.be>
2013-02-26 17:19:19 +0100

    Fixed some syntax.

--- src/asteroids/Position.java
+++ src/asteroids/Position.java
@@ -12,67 +12,72 @@ import be.kuleuven.cs.som.annotate.Raw;
  * @version 0.0
  */
 public class Position extends Vector {
-	
-	/**
-	 * Sets the x component of this position to the given x component.
-	 * 
-	 * @param 	x
-	 *          The new x-component for this velocity.
-	 * @post 	If this velocity can have the given x-component as its x-component,
-	 *       	then the x-component of this velocity is now equal to the given
-	 *       	x-component. | if canHaveAsXComponent(vx) | then
-	 *       	| new.getXComponent() == vx
-	 */
-	@Basic
-	@Raw
-	public void setXComponent(double x)
-	{
-		if (!canHaveAsComponent(x)) {
+    
+    /**
+     * Sets the x component of this position to the given x component.
+     * 
+     * @param   x
+     *          The new x-component for this velocity.
+     * @post    If this velocity can have the given x-component as its x-component,
+     *          then the x-component of this velocity is now equal to the given
+     *          x-component. | if canHaveAsXComponent(vx) | then
+     *          | new.getXComponent() == vx
+     */
+    @Basic
+    @Raw
+    public void setXComponent(double x)
+    {
+        if (!canHaveAsComponent(x)) {
             throw new IllegalArgumentException("Invalid vector component provided");
         } 
-        else {
-			this.x = x;
-		}
-	}
-	
-	/**
-	 * Sets the y component of this position to the given y component.
-	 * 
-	 * @param	y
-	 *          The new y-component for this velocity.
-	 * @post 	If this velocity can have the given y-component as its y-component,
-	 *       	then the y-component of this velocity is now equal to the given
-	 *       	y-component. | if canHaveAsYComponent(vy) | then
-	 *       	| new.getYComponent() == vy
-	 */
-	@Basic
-	@Raw
-	public void setYComponent(double y)
-	{
-		if (!canHaveAsComponent(y)) {
+        else 
+        {
+            this.x = x;
+        }
+    }
+    
+    /**
+     * Sets the y component of this position to the given y component.
+     * 
+     * @param   y
+     *          The new y-component for this velocity.
+     * @post    If this velocity can have the given y-component as its y-component,
+     *          then the y-component of this velocity is now equal to the given
+     *          y-component. | if canHaveAsYComponent(vy) | then
+     *          | new.getYComponent() == vy
+     */
+    @Basic
+    @Raw
+    public void setYComponent(double y)
+    {
+        if (!canHaveAsComponent(y)) {
             throw new IllegalArgumentException("Invalid vector component provided");
         } 
-        else {
-			this.y = y;
-		}
-	}
-	
-	/**
-	 * Initializes this new Position with a given x-coordinate and y-coordinate.
-	 * 
-	 * @param	x
-	 * 			The given x coordinate
-	 * @param	y
-	 * 			The given y coordinate
-	 */
-	public Position(double x,double y){
-		super(x,y);
-	}
-	
-	/**
-	 * Initializes this new Position at (0,0)
-	 */
-	public Position(){
-		super();
-	}
+        else 
+        {
+            this.y = y;
+        }
+    }
+    
+    /**
+     * Initializes this new Position with a given x-coordinate and y-coordinate.
+     * 
+     * @param   x
+     *          The given x coordinate
+     * @param   y
+     *          The given y coordinate
+     */
+    public Position(double x,double y)
+    {
+        super(x,y);
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see Vector#Position()
+     */
+    public Position()
+    {
+        super();
+    }
 }
