b0b0d5fb5f7bf15d8d204d76449554e693bc8bbb
Xavier Goás Aguililla <xavier@hal.local>
2013-03-22 12:44:18 +0100

    Completed documentation for Force class.

--- src/vector/Force.java
+++ src/vector/Force.java
@@ -8,16 +8,39 @@ package vector;
  */
 public class Force extends Vector
 {
+	/**
+   * The extended constructor for this class.
+   *
+	 * @param x  The x component of this force.
+	 * @param y  The y component of this force.
+	 * @effect 	 Initializes this acceleration by calling the extended vector constructor.
+	 * 			| Vector(x,y)
+	 */
 	public Force(double x, double y)
 	{
 		//TODO  
 	}
 
-	public Force(Vector v) throws IllegalArgumentException
+	/**
+   * The vector constructor for this class.
+   *
+	 * @param v  The vector we want to replicate as a force vector.
+	 * @effect	Initializes this acceleration by calling the by vector vector constructor.
+	 * 			| Vector(v)
+	 * @throws	NullPointerException
+	 * 			The given vector is null.
+	 * 			| v == null
+	 */
+	public Force(Vector v) throws NullPointerException
 	{
 		//TODO  
 	}
 
+	/**
+	 * Initializes this force vector with default values.
+   *
+	 * @effect	Initializes this acceleration by calling the simple vector constructor.
+	 */
 	public Force()
 	{
 		//TODO  
