d1f5842163ac2f4caf236516b5a8c2e263079748
Xavier Goás Aguililla <xavier@hal.local>
2013-03-22 10:42:59 +0100

    Fixed Mass package declarations.

--- src/entity/ship/Mass.java
+++ src/entity/ship/Mass.java
@@ -1,5 +1,7 @@
 package entity.ship;
-
+import model.Util;
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Raw;
 /**
  * 
  * @author Tom Sydney Kerckhove & Xavier Goas Aguililla
