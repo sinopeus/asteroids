8b2468fbd3504e05a8097dfbed4d37ac0f277464
Xavier Goás Aguililla <xavier@hal.local>
2013-03-22 12:25:13 +0100

    Added formal documentation.

--- src/Utilities/Newton.java
+++ src/Utilities/Newton.java
@@ -16,6 +16,7 @@ public class Newton
    * @param m2 The second mass.
    * @param a1 The first acceleration.
    * @return The second acceleration, as calculated from the arguments.
+   *         | return (a2.getScaledBy(m1.get() / m2.get()))
    * @throws IllegalArgumentException If the provided acceleration provided is equal to zero or any of the arguments are null objects.
    */
   public static Acceleration firstLaw_CalculateAcceleration(Mass m1, Mass m2, Acceleration a1) throws IllegalArgumentException
@@ -27,6 +28,7 @@ public class Newton
    * @param f The given force.
    * @param m The given mass.
    * @return  The acceleration corresponding to this force and mass.
+   *          | return (f.getScaledBy(m.get()))
    * @throws IllegalArgumentException If any of the arguments are null objects.
    */
     public static Acceleration secondLaw_CalculateAcceleration(Force f, Mass m) throws IllegalArgumentException
@@ -37,6 +39,7 @@ public class Newton
   /**
    * @param   f1  The given force.
    * @return  The opposite force.
+   *          | return f1.getScaledBy(-1.0)
    */
   public static Force thirdLaw_CalculateForce(Force f1){
     return f2;
