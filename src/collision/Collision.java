03ffc6f18db3328799c5a2e50795e351dce07b3f
NorfairKing <Syd.Kerckhove@gmail.com>
2013-03-26 10:59:59 +0100

    started on collisions

--- src/collision/Collision.java
+++ src/collision/Collision.java
@@ -1,6 +1,7 @@
 package collision;
 
 import vector.Position;
+import world.World;
 
 public abstract class Collision
 {
@@ -11,20 +12,44 @@ public abstract class Collision
 	}
 
 	//TODO EVERYTHING
-	public boolean canHaveAsPosition(Position position){
+	public boolean canHaveAsPosition(Position position)
+	{
 		return true;
 	}
-	
+
 	//TODO EVERYTHING
 	public void setCollisionPosition(Position collisionPosition)
 	{
 		this.collisionPosition = collisionPosition;
 	}
-	
+
 	private Position collisionPosition;
 
-	public abstract void resolve();
-	
 	//TODO EVERYTHING
-	public abstract double getTimeToCollision();
+	public double getTimeToCollision()
+	{
+		return this.timeToCollision;
+	}
+
+	//TODO EVERYTHING
+	protected boolean canHaveAsTimeToCollision(double timeToCollision)
+	{
+		return true;
+	}
+
+	//TODO EVERYTHING
+	protected void setTimeToCollision(double timeToCollision)
+	{
+		this.timeToCollision = timeToCollision;
+	}
+
+	private double timeToCollision;
+
+	public abstract void resolve();
+
+	//TODO EVERYTHING
+	public static Collision getNextCollision(World world)
+	{
+		return null;
+	}
 }
--- src/collision/EntityCollision.java
+++ src/collision/EntityCollision.java
@@ -1,13 +1,48 @@
 package collision;
 
+import entity.Entity;
+
 public class EntityCollision extends Collision
 {
+	//TODO EVERYTHING
+	public Entity getEntity1()
+	{
+		return entity1;
+	}
+
+	//TODO EVERYTHING
+	public Entity getEntity2()
+	{
+		return entity2;
+	}
+
+	//TODO EVERYTHING
+	public void setEntity1(Entity entity1)
+	{
+		this.entity1 = entity1;
+	}
+
+	//TODO EVERYTHING
+	public void setEntity2(Entity entity2)
+	{
+		this.entity2 = entity2;
+	}
+
+	//TODO EVERYTHING
+	private boolean canHaveAsEntity(Entity entity)
+	{
+		return true;
+	}
+
+	private Entity entity1;
+	private Entity entity2;
+
 	//TODO EVERYTHING
 	@Override
 	public void resolve()
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
