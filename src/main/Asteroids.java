d98c1334a4ec8739d31d09d912f6d755ff66f08b
NorfairKing <Syd.Kerckhove@gmail.com>
2013-03-28 14:43:46 +0100

    had some fun

--- src/main/Asteroids.java
+++ src/main/Asteroids.java
@@ -70,8 +70,20 @@ public class Asteroids <World, Ship, Asteroid, Bullet> extends JFrame
 		World world = facade.createWorld(width, height);
 		Ship player = facade.createShip(width / 2., height / 2., 0, 0, 40, 0, 5E15);
 		facade.addShip(world, player);
-		Asteroid asteroid1 = facade.createAsteroid(100, 100, 25, 50, 60);
+		Asteroid asteroid1 = facade.createAsteroid(200, 200, 25, 50, 20);
 		facade.addAsteroid(world, asteroid1);
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				for (int j = 0; j < i; j++)
+				{
+					Asteroid asteroid = facade.createAsteroid(1400 + 10 * i, 800 + 10 * j - 5 * i, 0, 0, 5);
+					facade.addAsteroid(world, asteroid);
+				}
+			}
+			Asteroid x = facade.createAsteroid(700, 800 - 10, 500, 0, 10);
+			facade.addAsteroid(world, x);
+		}
 		Asteroid asteroid2 = facade.createAsteroid(600, 80, -30, -40, 80);
 		facade.addAsteroid(world, asteroid2);
 		view = new WorldView <World, Ship, Asteroid, Bullet>(this, world, player, null);
