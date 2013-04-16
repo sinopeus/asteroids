1eca7cf3459a9259c7b079866a591e4d303d6bb7
NorfairKing <Syd.Kerckhove@gmail.com>
2013-04-16 17:03:59 +0200

    fixed tests

--- Test/main/AsteroidsTestSuite.java
+++ Test/main/AsteroidsTestSuite.java
@@ -15,6 +15,7 @@ import world.physics.MechanicsTest;
 import world.physics.collision.BorderCollisionTest;
 import world.physics.collision.CollisionTest;
 import world.physics.collision.EntityCollisionTest;
+import world.physics.collision.EventTest;
 import world.physics.geometry.AngleTest;
 import world.physics.geometry.CircleShapeTest;
 import world.physics.vector.AccelerationTest;
@@ -27,8 +28,8 @@ import world.physics.vector.VelocityTest;
 @SuppressWarnings ("javadoc")
 @RunWith (Suite.class)
 @SuiteClasses (
-{ BorderCollisionTest.class, CollisionTest.class, EntityCollisionTest.class, MassTest.class, ShipTest.class, ThrusterTest.class, AngleTest.class, AsteroidTest.class, BulletTest.class, CircleShapeTest.class, EntityTest.class, MechanicsTest.class, AccelerationTest.class, DirectionTest.class, ForceTest.class, PositionTest.class, VectorTest.class, VelocityTest.class, WorldTest.class })
+{EventTest.class, MassTest.class, ShipTest.class, ThrusterTest.class, AngleTest.class, AsteroidTest.class, BulletTest.class, CircleShapeTest.class, EntityTest.class, MechanicsTest.class, AccelerationTest.class, DirectionTest.class, ForceTest.class, PositionTest.class, VectorTest.class, VelocityTest.class, WorldTest.class })
 public class AsteroidsTestSuite
 {
-	
+
 }
--- Test/world/entity/ship/ShipTest.java
+++ Test/world/entity/ship/ShipTest.java
@@ -7,10 +7,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import world.World;
 import world.entity.Bullet;
-import world.entity.ship.Ship;
+import world.entity.Entity;
 import world.physics.Mass;
 import world.physics.Mechanics;
 import world.physics.geometry.Angle;
@@ -20,13 +19,13 @@ import world.physics.vector.Position;
 import world.physics.vector.Velocity;
 import Utilities.Util;
 
-@SuppressWarnings("javadoc")
+@SuppressWarnings ("javadoc")
 public class ShipTest
 {
 	@Before
-	public void setUpMutableTestFixture_Ship()
+	public void setUpMutableTestFixture_Ship ()
 	{
-		World w = new World(1000,1000);
+		World w = new World(1000, 1000);
 
 		Angle a = new Angle(Math.PI / 2);
 		Direction d = new Direction(a);
@@ -43,12 +42,12 @@ public class ShipTest
 		terminatedShip.terminate();
 	}
 
-	private static Ship testShip;
-	private static Ship terminatedShip;
-	private static Bullet testBullet;
+	private static Ship		testShip;
+	private static Ship		terminatedShip;
+	private static Bullet	testBullet;
 
 	@Test
-	public void extendedConstructorTest_FieldsMatchPerfectParameters()
+	public void extendedConstructorTest_FieldsMatchPerfectParameters ()
 	{
 		Angle a = new Angle(Math.PI / 2);
 		Direction d = new Direction(a);
@@ -66,21 +65,21 @@ public class ShipTest
 		assertEquals(ship.getMass(), m);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void extendedConstructorTest_IllegalPosition()
+	@Test (expected = IllegalArgumentException.class)
+	public void extendedConstructorTest_IllegalPosition ()
 	{
 		new Ship(new Direction(), null, 500, new Velocity(), new CircleShape(50), new Mass(40));
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void extendedConstructorTest_IllegalCircleShape()
+	@Test (expected = IllegalArgumentException.class)
+	public void extendedConstructorTest_IllegalCircleShape ()
 	{
 		CircleShape cs = new CircleShape(Ship.getMinimumRadius() - 1);
 		new Ship(new Direction(), new Position(), Velocity.getSpeedOfLight(), new Velocity(), cs, new Mass(40));
 	}
 
 	@Test
-	public void extendedConstructorTest_IllegalSpeedLimit()
+	public void extendedConstructorTest_IllegalSpeedLimit ()
 	{
 		double sl = Velocity.getSpeedOfLight() + 1;
 		Ship ship = new Ship(new Direction(), new Position(), sl, new Velocity(), new CircleShape(Ship.getMinimumRadius()), new Mass(40));
@@ -89,21 +88,21 @@ public class ShipTest
 	}
 
 	@Test
-	public void extendedConstructorTest_IllegalVelocity()
+	public void extendedConstructorTest_IllegalVelocity ()
 	{
 		Velocity v = new Velocity(Velocity.getSpeedOfLight(), Velocity.getSpeedOfLight());
 		Ship ship = new Ship(new Direction(), new Position(), Velocity.getSpeedOfLight(), v, new CircleShape(Ship.getMinimumRadius()), new Mass(40));
 		assertEquals(ship.getVelocity(), new Velocity(212132.03435596428, 212132.03435596428));
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void extendedConstructorTest_IllegalMass()
+	@Test (expected = IllegalArgumentException.class)
+	public void extendedConstructorTest_IllegalMass ()
 	{
 		new Ship(new Direction(), new Position(), 500, new Velocity(), new CircleShape(50), null);
 	}
 
 	@Test
-	public void simpleConstructorTest()
+	public void simpleConstructorTest ()
 	{
 		Ship ship = new Ship();
 		Direction d = new Direction();
@@ -120,64 +119,74 @@ public class ShipTest
 		assertEquals(ship.getMass(), m);
 
 	}
-	
+
 	@Test
-	public void canHaveAsShapeTest(){
+	public void canHaveAsShapeTest ()
+	{
 		assertTrue(testShip.canHaveAsShape(new CircleShape(50)));
 		assertFalse(testShip.canHaveAsShape(new CircleShape(5)));
 		assertFalse(testShip.canHaveAsShape(null));
 	}
-	
+
 	@Test
-	public void advanceTest_thrusterOff(){
-		Position newPosition = new Position(60,60);
+	public void advanceTest_thrusterOff ()
+	{
+		Position newPosition = new Position(60, 60);
 		testShip.advance(2);
 		assertEquals(newPosition, testShip.getPosition());
 	}
-	
+
 	@Test
-	public void advanceTest_thrusterOn(){
-		Position newPosition = new Position(50.5,52.7);
-		Velocity newVelocity = new Velocity(5,27.0);
+	public void advanceTest_thrusterOn ()
+	{
+		Position newPosition = new Position(50.5, 52.7);
+		Velocity newVelocity = new Velocity(5, 27.0);
 		testShip.getThruster().activate();
-		
+
 		testShip.advance(0.1);
-		
+
 		assertEquals(newPosition, testShip.getPosition());
 		assertEquals(newVelocity, testShip.getVelocity());
 	}
 
 	@Test
-	public void fireTest () {
+	public void fireTest ()
+	{
 		World testWorld = new World(1000, 1000);
 		testWorld.add(testShip);
 		testShip.fire();
-		assertTrue(testWorld.get(1).getClass() == Bullet.class);
-		Bullet b = (Bullet) testWorld.get(1);
-		assertEquals(testShip, b.getShooter());
+		testShip.terminate();
+		for (Entity e : testWorld)
+		{
+			assertTrue(e instanceof Bullet);
+			Bullet b = (Bullet) e;
+			assertEquals(testShip, b.getShooter());
+		}
 	}
-	
+
 	@Test
-	public void recoilTest () {
+	public void recoilTest ()
+	{
 		World testWorld = new World(1000, 1000);
 		testWorld.add(testShip);
-		
+
 		Velocity v = testShip.getVelocity();
 		Mass m = testShip.getMass();
-		Velocity bv =new Velocity(testShip.getVelocity().getSum(testShip.getDirection().getScaledBy(250)));
-		Mass bm = new Mass((4 * Math.PI * Math.pow(3, 3) * 7.8E10) / 3.0);
+		Velocity bv = new Velocity(testShip.getVelocity().getSum(testShip.getDirection().getScaledBy(250)));
+		Mass bm = new Mass( (4 * Math.PI * Math.pow(3, 3) * 7.8E10) / 3.0);
 		Velocity newShipVelocity = new Velocity(testShip.getVelocity().getDifference(Mechanics.conservationOfMomentum_CalculateVelocity(bv, bm, m)));
 
 		testShip.fire();
-		
-		assertEquals(testShip.getVelocity(),newShipVelocity);
+
+		assertEquals(testShip.getVelocity(), newShipVelocity);
 	}
-	
+
 	@Test
-	public void toStringTest(){
+	public void toStringTest ()
+	{
 		testShip.toString();
 	}
-	
+
 	//	@Test
 	//	public void thrustTest_PerfectParameters()
 	//	{
