528d5f93e45c2fc2ef080049ef717dca7b167f10
Xavier Goás Aguililla <xavier.goas@gmail.com>
2013-04-18 13:44:12 +0200

    Made collision resolution better.

--- src/world/entity/Asteroid.java
+++ src/world/entity/Asteroid.java
@@ -14,6 +14,7 @@ import world.physics.vector.Velocity;
  * 			| canHaveAsDirection(getDirection())
  * @invar	The position of this asteroid is a valid position.
  * 			| canHaveAsPosition(getPosition())
+
  * @invar	The shape of this asteroid is a valid shape.
  * 			| canHaveAsShape(getShape())
  * @invar	The velocity of this asteroid is a valid velocity.
@@ -23,102 +24,119 @@ import world.physics.vector.Velocity;
  */
 public class Asteroid extends Entity
 {
-	/**
-	 * Initializes this new asteroid with a given direction, position, shape, speed limit and velocity.
-	 * 
-	 * @param	direction
-	 * 			The given direction.
-	 * @param	position
-	 * 			The given position.
-	 * @param	shape
-	 * 			The given shape.
-	 * @param	velocity
-	 * 			The given velocity.
-	 * @param	shape
-	 * 			The given circle shape
-	 * @effect	The direction of this asteroid is set to the given direction.
-	 * 			| setDirection(direction)
-	 * @effect	The position of this asteroid is set to the given position.
-	 * 			| setPosition(position)
-	 * @effect	The shape of this asteroid is set to the given shape.
-	 * 			| setShape(shape)
-	 * @effect	The velocity of this asteroid is set to the given velocity.
-	 * 			| setVelocity(velocity)
-	 * @effect	The circle shape of this asteroid is set to the given circle shape.
-	 * 			| this.shape = shape
-	 * @effect	The mass of this asteroid is set to the its radius times the fixed density for asteroids.
-	 * 			| setMass(shape.getRadius() * density)
-	 * @throws	IllegalArgumentException
-	 * 			| Any of the parameters is null.
-		this.shooter = null;
+    /**
+     * Initializes this new asteroid with a given direction, position, shape, speed limit and velocity.
+     * 
+     * @param	direction
+     * 			The given direction.
+     * @param	position
+     * 			The given position.
+     * @param	shape
+     * 			The given shape.
+     * @param	velocity
+     * 			The given velocity.
+     * @param	shape
+     * 			The given circle shape
+     * @effect	The direction of this asteroid is set to the given direction.
+     * 			| setDirection(direction)
+     * @effect	The position of this asteroid is set to the given position.
+     * 			| setPosition(position)
+     * @effect	The shape of this asteroid is set to the given shape.
+     * 			| setShape(shape)
+     * @effect	The velocity of this asteroid is set to the given velocity.
+     * 			| setVelocity(velocity)
+     * @effect	The circle shape of this asteroid is set to the given circle shape.
+     * 			| this.shape = shape
+     * @effect	The mass of this asteroid is set to the its radius times the fixed density for asteroids.
+     * 			| setMass(shape.getRadius() * density)
+     * @throws	IllegalArgumentException
+     * 			| Any of the parameters is null.
+     this.shooter = null;
 	
-	 */
-	public Asteroid (Direction direction, Position position, Velocity velocity, CircleShape shape) throws IllegalArgumentException
-	{
-		super(direction, position, Velocity.getSpeedOfLight(), velocity, shape, new Mass( (4 * Math.PI * Math.pow(shape.getRadius(), 3) * density) / 3));
-	}
+    */
+    public Asteroid (Direction direction, Position position, Velocity velocity, CircleShape shape) throws IllegalArgumentException
+    {
+	super(direction, position, Velocity.getSpeedOfLight(), velocity, shape, new Mass( (4 * Math.PI * Math.pow(shape.getRadius(), 3) * density) / 3));
+    }
 
-	/**
-	 * Terminates the asteroid; if its dimensions were above a certain threshold, two new asteroids are spawned, each with half the radius of the terminated asteroid.
-	 *
-	 * @effect //TODO this is going to be really long.
-	 */
-	@Override
-	public void terminate ()
-	{
-		if (getShape().getRadius() >= hardness)
-		{
-			Direction d1 = new Direction(new Angle(getDirection().getAngle().get()));
-			Direction d2 = new Direction(new Angle(getDirection().getAngle().get() + Math.PI));
-			Position p1 = new Position(getPosition().getSum(d1.getScaledBy(getShape().getRadius() / 2)));
-			Position p2 = new Position(getPosition().getSum(d2.getScaledBy(getShape().getRadius() / 2)));
-			Velocity v1 = new Velocity(getVelocity().getScaledBy(1.5));
-			Velocity v2 = new Velocity(getVelocity().getScaledBy(-1.5));
-			CircleShape s1 = new CircleShape(getShape().getRadius() / 2);
-			CircleShape s2 = new CircleShape(getShape().getRadius() / 2);
+    /**
+     * Terminates the asteroid; if its dimensions were above a certain threshold, two new asteroids are spawned, each with half the radius of the terminated asteroid.
+     *
+     * @effect //TODO this is going to be really long.
+     */
+    @Override
+    public void terminate ()
+    {
+	if (getShape().getRadius() >= hardness)
+	    {
+		Direction d1 = new Direction(new Angle(getDirection().getAngle().get()));
+		Direction d2 = new Direction(new Angle(getDirection().getAngle().get() + Math.PI));
+		Position p1 = new Position(getPosition().getSum(d1.getScaledBy(getShape().getRadius() / 2)));
+		Position p2 = new Position(getPosition().getSum(d2.getScaledBy(getShape().getRadius() / 2)));
+		Velocity v1 = new Velocity(getVelocity().getScaledBy(1.5));
+		Velocity v2 = new Velocity(getVelocity().getScaledBy(-1.5));
+		CircleShape s1 = new CircleShape(getShape().getRadius() / 2);
+		CircleShape s2 = new CircleShape(getShape().getRadius() / 2);
 
-			Asteroid a1 = new Asteroid(d1, p1, v1, s1);
-			Asteroid a2 = new Asteroid(d2, p2, v2, s2);
+		Asteroid a1 = new Asteroid(d1, p1, v1, s1);
+		Asteroid a2 = new Asteroid(d2, p2, v2, s2);
 
-			getWorld().add(a1);
-			getWorld().add(a2);
-		}
-		super.terminate();
-	}
+		getWorld().add(a1);
+		getWorld().add(a2);
+	    }
+	super.terminate();
+    }
 
-	/**
-	 * Advances the asteroid's state by one turn. This happens in the same way as for a generic entity, except for a fixed turning rate per second.
-	 * 
-	 * @param dt	The time difference over which the state of the asteroid is advanced.
-	 * @effect		| this.turn(new Angle(Math.PI / period))
-	 * 				| super.advance(dt)
-	 * @see world.entity.Entity#advance(double)
-	 */
-	@Override
-	public void advance (double dt)
-	{
-		this.turn(new Angle(Math.PI / period * dt));
-		super.advance(dt);
-	}
+    /**
+     * Advances the asteroid's state by one turn. This happens in the same way as for a generic entity, except for a fixed turning rate per second.
+     * 
+     * @param dt	The time difference over which the state of the asteroid is advanced.
+     * @effect		| this.turn(new Angle(Math.PI / period))
+     * 				| super.advance(dt)
+     * @see world.entity.Entity#advance(double)
+     */
+    @Override
+    public void advance (double dt)
+    {
+	this.turn(new Angle(Math.PI / period * dt));
+	super.advance(dt);
+    }
 
-	@Override
-	public String toString ()
-	{
-		return "Asteroid_" + hashCode() + super.toString();
-	}
+    //TODO document & test
+    public void collideWith(Asteroid that) {
+	this.bounce(that);
+    }
 
-	/**
-	 * A variable registering the fixed period of all asteroids
-	 */
-	private final static double	period		= 7.0;
+    
+    //TODO document & test
+    public void collideWith(Bullet that) {
+	this.terminate();
+	that.terminate();
+    }
+    
+    //TODO document & test
+    public void collideWith(Ship that) {
+	that.terminate();
+    }
+    
+    @Override
+    public String toString ()
+    {
+	return "Asteroid_" + hashCode() + super.toString();
+    }
 
-	/**
-	 * A variable registering the fixed hardness of all asteroids.
-	 */
-	private final static double	hardness	= 30.0;
+    /**
+     * A variable registering the fixed period of all asteroids
+     */
+    private final static double	period		= 7.0;
 
-	/**
-	 * A variable registering the fixed density of all asteroids.
-	 */
-	private final static double	density		= 2.65E12;
+    /**
+     * A variable registering the fixed hardness of all asteroids.
+     */
+    private final static double	hardness	= 30.0;
+
+    /**
+     * A variable registering the fixed density of all asteroids.
+     */
+    private final static double	density		= 2.65E12;
 }
--- src/world/entity/ship/Ship.java
+++ src/world/entity/ship/Ship.java
@@ -31,183 +31,200 @@ import be.kuleuven.cs.som.annotate.Raw;
  */
 public class Ship extends Entity implements IShip
 {
-	/**
-	 * Initializes this new ship with a given direction, position, shape, mass, speed limit and velocity.
-	 * 
-	 * @param	direction
-	 * 			The given direction.
-	 * @param	position
-	 * 			The given position.
-	 * @param	shape
-	 * 			The given shape.
-	 * @param	speedLimit
-	 * 			The given speed limit.
-	 * @param	velocity
-	 * 			The given velocity
-	 * @param	shape
-	 * 			The given circle shape
-	 * @param	mass
-	 * 			The given mass
-	 * @effect	The direction of this ship is set to the given direction.
-	 * 			| setDirection(direction)
-	 * @effect	The position of this ship is set to the given position.
-	 * 			| setPosition(position)
-	 * @effect	The shape of this ship is set to the given shape.
-	 * 			| setShape(shape)
-	 * @effect	The speed limit of this ship is set to the given speed limit.
-	 * 			| setSpeedLimit(speedLimit)
-	 * @effect	The velocity of this ship is set to the given velocity.
-	 * 			| setVelocity(velocity)
-	 * @effect	The circle shape of this ship is set to the given circle shape.
-	 * 			| this.shape = shape
-	 * @effect	The mass of this ship is set to the given mass.
-	 * 			| setMass(mass)
-	 * @throws	IllegalArgumentException
-	 * 			| The given shape is not a legal shape.
-	 * @throws	NullPointerException
-	 * 			| Any of the parameters is null.
-	 */
-	public Ship (Direction direction, Position position, double speedLimit, Velocity velocity, CircleShape shape, Mass mass) throws IllegalArgumentException, NullPointerException
-	{
-		//TODO add throws from setters
-		super (direction, position, speedLimit, velocity, shape, mass);
-		setThruster (new Thruster (getThrustPerSecond (), this));
+    /**
+     * Initializes this new ship with a given direction, position, shape, mass, speed limit and velocity.
+     * 
+     * @param	direction
+     * 			The given direction.
+     * @param	position
+     * 			The given position.
+     * @param	shape
+     * 			The given shape.
+     * @param	speedLimit
+     * 			The given speed limit.
+     * @param	velocity
+     * 			The given velocity
+     * @param	shape
+     * 			The given circle shape
+     * @param	mass
+     * 			The given mass
+     * @effect	The direction of this ship is set to the given direction.
+     * 			| setDirection(direction)
+     * @effect	The position of this ship is set to the given position.
+     * 			| setPosition(position)
+     * @effect	The shape of this ship is set to the given shape.
+     * 			| setShape(shape)
+     * @effect	The speed limit of this ship is set to the given speed limit.
+     * 			| setSpeedLimit(speedLimit)
+     * @effect	The velocity of this ship is set to the given velocity.
+     * 			| setVelocity(velocity)
+     * @effect	The circle shape of this ship is set to the given circle shape.
+     * 			| this.shape = shape
+     * @effect	The mass of this ship is set to the given mass.
+     * 			| setMass(mass)
+     * @throws	IllegalArgumentException
+     * 			| The given shape is not a legal shape.
+     * @throws	NullPointerException
+     * 			| Any of the parameters is null.
+     */
+    public Ship (Direction direction, Position position, double speedLimit, Velocity velocity, CircleShape shape, Mass mass) throws IllegalArgumentException, NullPointerException
+    {
+	//TODO add throws from setters
+	super (direction, position, speedLimit, velocity, shape, mass);
+	setThruster (new Thruster (getThrustPerSecond (), this));
+    }
+
+    /**
+     * Initializes this new ship with default values.
+     * 
+     * @effect	Initializes this new ship with the extended ship constructor and default values.
+     * 			| this(new Direction(),new Position(),Velocity.getSpeedOfLight(),new Velocity(), new CircleShape(40), new Mass(5E15))
+     */
+    public Ship ()
+    {
+	//TODO add throws from setters
+	this (new Direction (), new Position (), Velocity.getSpeedOfLight (), new Velocity (), new CircleShape (40), new Mass (5E15));
+    }
+
+    /**
+     * @see world.entity.Entity#canHaveAsShape(world.physics.geometry.CircleShape)
+     */
+    @Override
+    protected boolean canHaveAsShape (@Raw CircleShape shape)
+    {
+	return (super.canHaveAsShape (shape) && (shape.getRadius () >= Ship.getMinimumRadius ()));
+    }
+
+    /**
+     * Returns the minimum radius of ships.
+     */
+    @SuppressWarnings ("javadoc")
+    @Basic
+    @Raw
+    public static double getMinimumRadius ()
+    {
+	return Ship.minimumRadius;
+    }
+
+    /**
+     * A variable registering the minimum radius of this ship.
+     */
+    private static double	minimumRadius	= 10;
+
+    /**
+     * Gets the thruster of this ship.
+     */
+    @Basic
+    @Raw
+    public Thruster getThruster ()
+    {
+	return thruster;
+    }
+
+    /**
+     * Checks whether this ship can have the given thruster as its thruster.
+     * 
+     * @param	thruster
+     * 			The given thruster
+     * @return	True if and only if the given thruster is not null.
+     * 			| result == (thruster != null)
+     */
+    private boolean canHaveAsThruster (Thruster thruster)
+    {
+	return (thruster != null);
+    }
+
+    /**
+     * Sets the thrust of this ship to the given thruster.
+     * 
+     * @param	thruster
+     * 			The given thruster.
+     * @throws	IllegalArgumentException
+     * 			The given thruster is null.
+     * 			| thruster == null
+     */
+    private void setThruster (Thruster thruster) throws IllegalArgumentException
+    {
+	if (!canHaveAsThruster (thruster)) { throw new IllegalArgumentException ("Invalid thruster provided."); }
+	this.thruster = thruster;
+    }
+
+    /**
+     * A variable referencing the thruster of this ship.
+     */
+    private Thruster	thruster;
+
+    /**
+     * Terminates this ship.
+     */
+    @Override
+    public void terminate ()
+    {
+	getThruster ().terminate ();
+	super.terminate ();
+    }
+
+    /**
+     * @see world.entity.Entity#advance(double)
+     */
+    @Override
+    public void advance (double dt)
+    {
+	if (getThruster ().isActivated ())
+	    {
+		getThruster ().thrust (dt);
+	    }
+	super.advance (dt);
+    }
+
+    //TODO document & test
+    public void collideWith(Asteroid that) {
+	this.terminate();
+    }
+
+    
+    //TODO document & test
+    public void collideWith(Bullet that) {
+	if (that.getShooter() == this) {
+	    this.bounce(that);
+	    return;
 	}
+	
+	this.terminate();
+	that.terminate();
+    }
+    
+    /**
+     * A method for firing a bullet from this ship.
+     * 
+     * @effect Creates a new Bullet object in the World to which this Ship belongs.
+     */
+    public void fire ()
+    {
+	Bullet b = new Bullet (this);
+	getWorld ().add (b);
 
-	/**
-	 * Initializes this new ship with default values.
-	 * 
-	 * @effect	Initializes this new ship with the extended ship constructor and default values.
-	 * 			| this(new Direction(),new Position(),Velocity.getSpeedOfLight(),new Velocity(), new CircleShape(40), new Mass(5E15))
-	 */
-	public Ship ()
-	{
-		//TODO add throws from setters
-		this (new Direction (), new Position (), Velocity.getSpeedOfLight (), new Velocity (), new CircleShape (40), new Mass (5E15));
-	}
+	//RECOIL
+	Velocity recoil = Mechanics.conservationOfMomentum_CalculateVelocity (b.getVelocity (), b.getMass (), this.getMass ());
+	this.setVelocity (new Velocity (getVelocity ().getDifference (recoil)));//TODO something is wrong.
+    }
 
-	/**
-	 * @see world.entity.Entity#canHaveAsShape(world.physics.geometry.CircleShape)
-	 */
-	@Override
-	protected boolean canHaveAsShape (@Raw CircleShape shape)
-	{
-		return (super.canHaveAsShape (shape) && (shape.getRadius () >= Ship.getMinimumRadius ()));
-	}
+    @Override
+    public String toString ()
+    {
+	return "Ship_" + hashCode () + super.toString ();
+    }
 
-	/**
-	 * Returns the minimum radius of ships.
-	 */
-	@SuppressWarnings ("javadoc")
-	@Basic
-	@Raw
-	public static double getMinimumRadius ()
-	{
-		return Ship.minimumRadius;
-	}
+    /**
+     * @return
+     */
+    public double getThrustPerSecond ()
+    {
+	return Ship.thrustPerSecond;
+    }
 
-	/**
-	 * A variable registering the minimum radius of this ship.
-	 */
-	private static double	minimumRadius	= 10;
-
-	/**
-	 * Gets the thruster of this ship.
-	 */
-	@Basic
-	@Raw
-	public Thruster getThruster ()
-	{
-		return thruster;
-	}
-
-	/**
-	 * Checks whether this ship can have the given thruster as its thruster.
-	 * 
-	 * @param	thruster
-	 * 			The given thruster
-	 * @return	True if and only if the given thruster is not null.
-	 * 			| result == (thruster != null)
-	 */
-	private boolean canHaveAsThruster (Thruster thruster)
-	{
-		return (thruster != null);
-	}
-
-	/**
-	 * Sets the thrust of this ship to the given thruster.
-	 * 
-	 * @param	thruster
-	 * 			The given thruster.
-	 * @throws	IllegalArgumentException
-	 * 			The given thruster is null.
-	 * 			| thruster == null
-	 */
-	private void setThruster (Thruster thruster) throws IllegalArgumentException
-	{
-		if (!canHaveAsThruster (thruster)) { throw new IllegalArgumentException ("Invalid thruster provided."); }
-		this.thruster = thruster;
-	}
-
-	/**
-	 * A variable referencing the thruster of this ship.
-	 */
-	private Thruster	thruster;
-
-	/**
-	 * Terminates this ship.
-	 */
-	@Override
-	public void terminate ()
-	{
-		getThruster ().terminate ();
-		super.terminate ();
-	}
-
-	/**
-	 * @see world.entity.Entity#advance(double)
-	 */
-	@Override
-	public void advance (double dt)
-	{
-		if (getThruster ().isActivated ())
-		{
-			getThruster ().thrust (dt);
-		}
-		super.advance (dt);
-	}
-
-	/**
-	 * A method for firing a bullet from this ship.
-	 * 
-	 * @effect Creates a new Bullet object in the World to which this Ship belongs.
-	 */
-	public void fire ()
-	{
-		Bullet b = new Bullet (this);
-		getWorld ().add (b);
-
-		//RECOIL
-		Velocity recoil = Mechanics.conservationOfMomentum_CalculateVelocity (b.getVelocity (), b.getMass (), this.getMass ());
-		this.setVelocity (new Velocity (getVelocity ().getDifference (recoil)));//TODO something is wrong.
-	}
-
-	@Override
-	public String toString ()
-	{
-		return "Ship_" + hashCode () + super.toString ();
-	}
-
-	/**
-	 * @return
-	 */
-	public double getThrustPerSecond ()
-	{
-		return Ship.thrustPerSecond;
-	}
-
-	/**
-	 * A variable registering the thrust that a ship's thruster can exert in one second.
-	 */
-	private static double	thrustPerSecond	= 1.1E19;
+    /**
+     * A variable registering the thrust that a ship's thruster can exert in one second.
+     */
+    private static double	thrustPerSecond	= 1.1E19;
 }
