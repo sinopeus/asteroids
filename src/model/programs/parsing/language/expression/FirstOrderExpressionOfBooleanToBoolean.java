65b47e045057d9b0491207052b6824c3f4d9dbde
Xavier Goás Aguililla <xavier.goas@gmail.com>
2013-05-13 07:23:59 +0200

    Fixed some spelling mistakes.

---
+++ src/model/programs/parsing/language/expression/FirstOrderExpressionOfBooleanToBoolean.java
@@ -0,0 +1,26 @@
+package model.programs.parsing.language.expression;
+
+
+public abstract class FirstOrderExpressionOfBooleanToBoolean extends FirstOrderExpression
+{
+	public FirstOrderExpressionOfBooleanToBoolean (int line, int column, Expression argument)
+	{
+		super(line, column, argument);
+	}
+
+	@Override
+	protected boolean canHaveAsArgument (Expression argument)
+	{
+		if (!super.canHaveAsArgument(argument)) return false;
+		//		return argument.evaluate() instanceof BooleanLiteral; //TODO
+		return true;
+	}
+
+	protected abstract BooleanLiteral function (BooleanLiteral argument);
+
+	@Override
+	public BooleanLiteral evaluate ()
+	{
+		return function((BooleanLiteral) getArgument().evaluate());
+	}
+}
