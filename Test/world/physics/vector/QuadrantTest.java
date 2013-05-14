80480a246ed1ed2336595371c338e27cb96083e1
Xavier Goás Aguililla <xavier.goas@gmail.com>
2013-05-14 16:22:35 +0200

    Added lots of test classes. Gotta fix this.

--- Test/world/physics/vector/QuadrantTest.java
+++ Test/world/physics/vector/QuadrantTest.java
@@ -1,16 +1,12 @@
-package world.physics.vector;
-
-import org.junit.Test;
-
-public class QuadrantTest
-{
-	@SuppressWarnings ("unused")
-	@Test
-	public void quadrantTest ()
-	{
-		Quadrant q1 = Quadrant.QUADRANT_I;
-		Quadrant q2 = Quadrant.QUADRANT_II;
-		Quadrant q3 = Quadrant.QUADRANT_III;
-		Quadrant q4 = Quadrant.QUADRANT_IV;
-	}
-}
+@SuppressWarnings("javadoc")
+public class QuadrantTest.java
+\{
+\t@Before
+public void setUp ()
+\{
+\}
+\t@Test
+public void Test ()
+\{
+\}
+\}
