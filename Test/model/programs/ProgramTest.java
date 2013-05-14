bf465991b6b4e4e756a48e8bbfb503aef808e2c2
Xavier Goás Aguililla <xavier.goas@gmail.com>
2013-05-14 17:38:29 +0200

    Added test stubs.

--- Test/model/programs/ProgramTest.java
+++ Test/model/programs/ProgramTest.java
@@ -3,16 +3,112 @@ package model.programs;
 import org.junit.Before;
 import org.junit.Test;
 
+
 @SuppressWarnings ("javadoc")
 public class ProgramTest
 {
 	@Before
-	public void setUp ()
+	public void setUpTestFixture  ()
 	{
+		//TODO
 	}
 
 	@Test
-	public void Test ()
+	public void constructorTest_perfectParams ()
+	{//TODO
+	}
+	
+	@Test
+	public void constructorTest_nullMap ()
 	{
+		//TODO
+	}
+	
+	@Test
+	public void constructorTest_nullStatement()
+	{
+		//TODO
+	}
+	
+	@Test
+	public void setGlobalTypesTest_perfectParams () {
+		//TODO
+	}
+	
+	@Test
+	public void setGlobalTypesTest_nullMap () {
+		//TODO
+	}
+	
+	
+	@Test
+	public void setGlobalValuesTest_perfectParams () {
+		//TODO
+	}
+	
+	@Test
+	public void setGlobalValuesTest_nullMap () {
+		//TODO
+	}
+	
+	@Test
+	public void canHaveAsGlobalsTest () {
+		//TODO
+	}
+	
+	@Test
+	public void canHaveAsStatementTest () {
+		//TODO
+	}
+	
+	@Test
+	public void setStatementTest_perfectParams () {
+		//TODO
+	}
+	
+	@Test
+	public void setStatementTest_nullParams () {
+		//TODO
+	}
+	
+	
+	@Test
+	public void canHaveAsOwnerTest () {
+		//TODO
+	}
+	
+	@Test
+	public void setOwnerTest () {
+		//TODO
+	}
+	
+	@Test
+	public void getVariableNamedTest_perfectParams () {
+		//TODO
+	}
+	
+	@Test
+	public void getVariableNamedTest_nullParams () {
+		//TODO
+	}
+	
+	@Test
+	public void setVariableValueTest_perfectParams () {
+		//TODO
+	}
+	
+	@Test
+	public void setVariableValueTest_nullValue () {
+		//TODO
+	}
+	
+	@Test
+	public void setVariableValueTest_nullName () {
+		//TODO
+	}
+	
+	@Test
+	public void executeUntilAfterNextActionTest () {
+		//TODO
 	}
 }
