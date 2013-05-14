16afd50bb835d30d2b9f60dae9a8728661ae302f
NorfairKing <Syd.Kerckhove@gmail.com>
2013-05-14 17:23:28 +0200

    fixed tests

--- Test/model/programs/parsing/language/expression/second_order/LTTest.java
+++ Test/model/programs/parsing/language/expression/second_order/LTTest.java
@@ -1,12 +1,18 @@
-@SuppressWarnings("javadoc")
-public class LTTest.java
+package model.programs.parsing.language.expression.second_order;
+
+import org.junit.Before;
+import org.junit.Test;
+
+@SuppressWarnings ("javadoc")
+public class LTTest
 {
-t@Before
-public void setUp ()
-{
-}
-t@Test
-public void Test ()
-{
-}
+	@Before
+	public void setUp ()
+	{
+	}
+
+	@Test
+	public void Test ()
+	{
+	}
 }
