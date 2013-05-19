6bc3964753fb23fd5440d4afbc2c43f2e3c50e36
Xavier Goás Aguililla <xavier.goas@gmail.com>
2013-05-19 17:29:02 +0200

    toString shenanigans

---
+++ src/model/programs/parsing/language/expression/second_order/Subtraction.java
@@ -0,0 +1,27 @@
+package model.programs.parsing.language.expression.second_order;
+
+import model.programs.parsing.language.ProgramException;
+import model.programs.parsing.language.expression.Expression;
+import model.programs.parsing.language.expression.constant.literal.DoubleLiteral;
+
+public class Subtraction extends SecondOrderExpressionOfNumbersToNumber
+{
+	public Subtraction (int line, int column, Expression firstArgument, Expression secondArgument) throws ProgramException
+	{
+		super(line, column, firstArgument, secondArgument);
+	}
+
+	@Override
+	protected DoubleLiteral function (DoubleLiteral first, DoubleLiteral second)
+	{
+		try
+		{
+			return new DoubleLiteral(getLine(), getColumn(), (first.getValue() - second.getValue()));
+		} catch (ProgramException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
